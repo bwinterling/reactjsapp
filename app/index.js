3b01f61551eec3b48e2ed359bd3129af67ad36d0
Brian Winterling <bwinterling@itriagehealth.com>
2016-04-29 16:55:22 -0600

    Hook up the ReactRouter and add components

--- app/index.js
+++ app/index.js
@@ -1,49 +1,63 @@
-var USER_DATA = {
-  name: 'Brian Winterling',
-  username: 'bwinterling',
-  image: 'https://avatars2.githubusercontent.com/u/4154768?v=3&s=460'
-}
+// var USER_DATA = {
+//   name: 'Brian Winterling',
+//   username: 'bwinterling',
+//   image: 'https://avatars2.githubusercontent.com/u/4154768?v=3&s=460'
+// }
 
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var ProfilePic = React.createClass({
-  render: function () {
-    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-  }
-});
+// var ProfilePic = React.createClass({
+//   render: function () {
+//     return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+//   }
+// });
 
-var ProfileLink = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <a href={'https://github.com/' + this.props.username}>
-          {this.props.username}
-        </a>
-      </div>
-    )
-  }
-});
+// var Link = React.createClass({
+//   changeURL: function () {
+//     window.location.replace(this.props.href)
+//   },
+//   render: function () {
+//     return (
+//       <span style={{color: 'blue', cursor: 'pointer'}} onClick={this.changeURL}>
+//         {this.props.children}
+//       </span>
+//     )
+//   }
+// });
 
-var ProfileName = React.createClass({
-  render: function () {
-    return <div>{this.props.name}</div>
-  }
-});
+// var ProfileLink = React.createClass({
+//   render: function () {
+//     return (
+//       <div>
+//         <Link href={'https://github.com/' + this.props.username}>
+//           {this.props.username}
+//         </Link>
+//       </div>
+//     )
+//   }
+// });
 
-var Avatar = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    )
-  }
-});
+// var ProfileName = React.createClass({
+//   render: function () {
+//     return <div>{this.props.name}</div>
+//   }
+// });
+
+// var Avatar = React.createClass({
+//   render: function () {
+//     return (
+//       <div>
+//         <ProfilePic imageUrl={this.props.user.image} />
+//         <ProfileName name={this.props.user.name} />
+//         <ProfileLink username={this.props.user.username} />
+//       </div>
+//     )
+//   }
+// });
 
 ReactDOM.render(
-  <Avatar user={USER_DATA} />,
+  routes,
   document.getElementById('app')
 );
